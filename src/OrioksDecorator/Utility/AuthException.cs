0724f2011594ddc4dfb37f8bca3f097cb678411e
Egor Fedorov <egor.fedorov@codestetic.com>
2022-04-06 14:40:03 +0300

    Обработка ситации неверного ввода логина-пароля, отсутствия токена, доступ к преподам и расписанию без авторизации

---
+++ src/OrioksDecorator/Utility/AuthException.cs
@@ -0,0 +1,12 @@
+﻿namespace OrioksDecorator.Utility
+{
+    /// <summary>
+    ///     Ошибка авторизации
+    /// </summary>
+    internal sealed class AuthException : Exception
+    {
+        /// <inheritdoc cref="AuthException"/>
+        public AuthException(string? message = "Wrong login or password") : base(message)
+        {}
+    }
+}
